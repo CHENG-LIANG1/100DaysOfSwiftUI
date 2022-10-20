d1af8e1d86e8c2c3a565afc2d40b208f60d77713
梁程 <liangcheng@holyblade.com>
2022-10-20 22:26:12 +0800

    Created core data project

---
+++ CoreDataProject/CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Cheng Liang(Louis) on 2022/10/20.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
