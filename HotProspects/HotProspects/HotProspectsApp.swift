aff1a8fc57fc0b3dd91aef4a5abb8328c50a82f2
梁程 <liangcheng@holyblade.com>
2022-11-06 16:00:00 +0800

    created project Hot Prospects

---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Cheng Liang(Louis) on 2022/11/6.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
